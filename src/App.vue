8aa29c818eafb01ae2806a047e4839283b4fd8f3
lbDung <lbdung.gli@vnpt.vn>
2021-12-23 16:40:57 +0700

    update tab report

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,53 @@
 <template>
   <div class="VueXlsx">
-    <section>
-      <section>
-        <h2>Import Báo Cáo</h2>
-        <input type="file" @change="onChange" class="mb-5" />
-        <xlsx-read :file="file">
-          <template #default="{ loading }">
-            <span v-if="loading">Loading...</span>
-            <xlsx-sheets>
-              <template #default="{ sheets }">
-                <label style="margin-right: 5px">Chọn sheet</label>
-                <select v-model="selectedSheet" class="select-sheet">
-                  <option value="">Chọn sheet</option>
-                  <option v-for="sheet in sheets" :key="sheet" :value="sheet">
-                    {{ sheet }}
-                  </option>
-                </select>
-              </template>
-            </xlsx-sheets>
-            <xlsx-json
-              :sheet="selectedSheet"
-              @parsed="updateColecction"
-              @table="returnTable"
-            >
-            </xlsx-json>
-          </template>
-        </xlsx-read>
-      </section>
-      <hr />
-      <h2>Xuất Báo Cáo</h2>
-
-      <!-- <div class="mb-5">Dữ liệu : {{ sheets }}</div> -->
-      <table class="mb-5 table">
-        <thead>
-          <tr>
-            <th>{{ tableTitle }}</th>
-            <th>Dưới 5</th>
-            <th>Từ 5 đến 6.5</th>
-            <th>Từ 6.5 đến 8</th>
-            <th>Từ 8 đến 10</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="item in table" :key="item">
-            <td>{{ item.a }}</td>
-            <td>{{ item.b }}</td>
-            <td>{{ item.c }}</td>
-            <td>{{ item.d }}</td>
-            <td>{{ item.e }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <xlsx-workbook>
-        <xlsx-sheet
-          :collection="sheet.data"
-          v-for="sheet in sheets"
-          :key="sheet.name"
-          :sheet-name="sheet.name"
-        />
-        <xlsx-sheet
-          :collection="jsonFile"
-          v-if="jsonFile"
-          sheet-name="fromJson"
-        />
-        <xlsx-download @done="updateColecction">
-          <button class="button-77" role="button">Tải về</button>
-        </xlsx-download>
-      </xlsx-workbook>
-    </section>
+    <v-card height="100%" class="big-card">
+      <v-tabs v-model="tab" background-color="primary" dark>
+        <v-tab v-for="item in items" :key="item.tab">
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in items" :key="item.tab">
+          <v-card height="100%" class="big-card">
+            <v-card-text>
+              <component v-bind:is="item.content"></component>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
   </div>
 </template>
 
 <script>
-import XlsxRead from "./components/XlsxRead";
-import XlsxSheets from "./components/XlsxSheets";
-import XlsxJson from "./components/XlsxJson";
-import XlsxWorkbook from "./components/XlsxWorkbook";
-import XlsxSheet from "./components/XlsxSheet";
-import XlsxDownload from "./components/XlsxDownload";
+import BAOCAO1 from "./BAOCAO1";
+import BAOCAO2 from "./BAOCAO2";
 
 export default {
   components: {
-    XlsxRead,
-    XlsxSheets,
-    XlsxJson,
-    XlsxWorkbook,
-    XlsxSheet,
-    XlsxDownload,
+    BAOCAO1,
+    BAOCAO2,
   },
   data() {
     return {
-      file: null,
-      selectedSheet: null,
-      sheetName: "Dữ liệu",
-      sheets: [],
-      collection: [],
-      jsonFile: null,
-      table: [],
       tableTitle: "Cuộc thi",
+      tab: null,
+      items: [
+        { tab: "Thống kê kết quả thi", content: "BAOCAO1" },
+        { tab: "Thống kê tham gia thi", content: "BAOCAO2" },
+      ],
     };
   },
-  methods: {
-    onChange(event) {
-      this.file = event.target.files ? event.target.files[0] : null;
-      this.collection = [];
-      this.table = [];
-      this.sheets = [];
-      this.tableTitle = "Cuộc Thi";
-    },
-    onJsonFileChange(event) {
-      const reader = new FileReader();
-      reader.onload = (evt) => {
-        console.log(this.jsonFile);
-        this.jsonFile = JSON.parse(evt.target.result);
-      };
-      reader.readAsText(event.target.files[0]);
-    },
-    updateColecction(data) {
-      this.collection = data;
-      this.sheets = [];
-      this.addSheet();
-    },
-    returnTable(data) {
-      this.table = data.returnTable;
-      this.tableTitle = data.title;
-    },
-    addSheet() {
-      this.sheets.push({ name: this.sheetName, data: [...this.collection] });
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style></style>
+<style>
+.big-card {
+  /* background-image: #d0d4f1 !important */
+  background-image: url("../public/bg.jpg");
+}
+.v-slide-group__content.v-tabs-bar__content {
+  background-color: darkslategray;
+}
+</style>
---
+++ src/BAOCAO2.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <section>
+      <section class="mb-5">
+        <h2 class="mb-5">Import Báo Cáo</h2>
+        <input type="file" @change="onChange" class="mb-5" />
+        <xlsx-read :file="file">
+          <template #default="{ loading }">
+            <span v-if="loading">Loading...</span>
+            <xlsx-sheets>
+              <template #default="{ sheets }">
+                <label style="margin-right: 5px">Chọn sheet</label>
+                <select v-model="selectedSheet" class="select-sheet">
+                  <option value="">Chọn sheet</option>
+                  <option v-for="sheet in sheets" :key="sheet" :value="sheet">
+                    {{ sheet }}
+                  </option>
+                </select>
+              </template>
+            </xlsx-sheets>
+            <xlsx-json
+              :sheet="selectedSheet"
+              @parsed="updateColecction"
+              @table="returnTable"
+            >
+            </xlsx-json>
+          </template>
+        </xlsx-read>
+      </section>
+      <hr class="mb-5" />
+      <h2 class="mb-5">Xuất Báo Cáo</h2>
+
+      <!-- <div class="mb-5">Dữ liệu : {{ sheets }}</div> -->
+      <table class="mb-5 table">
+        <thead>
+          <tr>
+            <th>{{ tableTitle }}</th>
+            <th>Tổng số học sinh</th>
+            <th>Có tham gia thi</th>
+            <th>Vắng thi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in table" :key="item.a">
+            <td>{{ item.a }}</td>
+            <td>{{ item.b }}</td>
+            <td>{{ item.c }}</td>
+            <td>{{ item.d }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h2 class="mb-5">Danh sách vắng</h2>
+      <table class="mb-5 table">
+        <thead>
+          <tr>
+            <th>Số thứ tự</th>
+            <th>Họ tên</th>
+            <th>Cuộc thi</th>
+            <th>Lớp</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in listVangThi" :key="item.__EMPTY_2">
+            <td>{{ index + 1 }}</td>
+            <td>{{ item.__EMPTY_2 }}</td>
+            <td>{{ item.__EMPTY }}</td>
+            <td>{{ item.__EMPTY_4 }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <xlsx-workbook>
+        <xlsx-sheet
+          :collection="sheet.data"
+          v-for="sheet in sheets"
+          :key="sheet.name"
+          :sheet-name="sheet.name"
+        />
+        <xlsx-sheet
+          :collection="jsonFile"
+          v-if="jsonFile"
+          sheet-name="fromJson"
+        />
+        <xlsx-download @done="updateColecction">
+          <button class="button-77" role="button">Tải về</button>
+        </xlsx-download>
+      </xlsx-workbook>
+    </section>
+  </div>
+</template>
+
+<script>
+import XlsxRead from "./components/XlsxRead";
+import XlsxSheets from "./components/XlsxSheets";
+import XlsxJson from "./components/XlsxJson2";
+import XlsxWorkbook from "./components/XlsxWorkbook";
+import XlsxSheet from "./components/XlsxSheet";
+import XlsxDownload from "./components/XlsxDownload";
+
+export default {
+  components: {
+    XlsxRead,
+    XlsxSheets,
+    XlsxJson,
+    XlsxWorkbook,
+    XlsxSheet,
+    XlsxDownload,
+  },
+  data() {
+    return {
+      file: null,
+      selectedSheet: null,
+      sheetName: "Dữ liệu",
+      sheets: [],
+      collection: [],
+      jsonFile: null,
+      table: [],
+      tableTitle: "Cuộc thi",
+      saiMau: false,
+      listVangThi: [],
+    };
+  },
+  watch: {
+    saiMau: function () {
+      if (this.saiMau) {
+        this.sheets = [];
+        alert("Sai mẫu Excel !!!!");
+      }
+    },
+  },
+  methods: {
+    onChange(event) {
+      this.file = event.target.files ? event.target.files[0] : null;
+      this.collection = [];
+      this.table = [];
+      this.sheets = [];
+      this.saiMau = false;
+      this.tableTitle = "Cuộc Thi";
+    },
+    onJsonFileChange(event) {
+      const reader = new FileReader();
+      reader.onload = (evt) => {
+        console.log(this.jsonFile);
+        this.jsonFile = JSON.parse(evt.target.result);
+      };
+      reader.readAsText(event.target.files[0]);
+    },
+    updateColecction(data) {
+      this.collection = data;
+      this.sheets = [];
+      this.addSheet();
+    },
+    returnTable(data) {
+      this.table = data.returnTable;
+      this.tableTitle = data.title;
+      this.saiMau = data.saiMau;
+      this.listVangThi = data.listVangThi;
+    },
+    addSheet() {
+      this.sheets.push({ name: this.sheetName, data: this.collection });
+    },
+  },
+};
+</script>
+
+<style></style>
